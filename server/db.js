598ce0e52d8167a449b618114361c970e5e998e6
cryptotweezer <cryptotweezer@gmail.com>
2025-04-25 05:25:07 -0400

    Update MongoDB URI with lab credentials and retry logic

---
+++ server/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://root:7ORNzoj7u9kGKEuw8gcFFyC5@172.21.19.27:27017";
+
+
+const connectToMongo = async (retryCount) => {
+    const MAX_RETRIES = 3;
+    const count = retryCount ?? 0;
+    try {
+        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
+        console.info('Connected to Mongo Successfully')
+
+        return;
+    } catch (error) {
+        console.error(error);
+
+        const nextRetryCount = count + 1;
+
+        if (nextRetryCount >= MAX_RETRIES) {
+            throw new Error('Unable to connect to Mongo!');
+        }
+
+        console.info(`Retrying, retry count: ${nextRetryCount}`)
+
+        return await connectToMongo(nextRetryCount);
+
+    }
+};
+
+module.exports = connectToMongo;
