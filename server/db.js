f3b875082794301e56b06b1544ac221b7d6a55fd
cryptotweezer <cryptotweezer@gmail.com>
2025-04-25 23:27:41 -0400

    Build the Appointment Booking Component

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://root:3w3aIqnORsNnz0eCthPAkdOD@172.21.98.81:27017";
+const mongoURI = "mongodb://root:YlAMyicT7E7yPFRQjxPZlqN3@172.21.188.42:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/signup" element={<Sign_Up />} />
         <Route path="/login" element={<Login />} />
         <Route path="/instant-consultation" element={<InstantConsultation />} />
+        <Route path="/find-doctor" element={<FindDoctorSearch />} />
       </Routes>
     </BrowserRouter>
   );
