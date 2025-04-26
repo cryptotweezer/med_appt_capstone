e163f675e70239702b66c4958bf10adba535643d
cryptotweezer <cryptotweezer@gmail.com>
2025-04-25 22:02:45 -0400

    Integrate Existing Functionality

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://root:BXWMYMRH47BBd7RiOlxkZJxY@172.21.22.181:27017";
+const mongoURI = "mongodb://root:3w3aIqnORsNnz0eCthPAkdOD@172.21.98.81:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
